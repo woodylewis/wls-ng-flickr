60295fe11091a60fddba5aa5249697c2b14570d4
woodylewis <woody@woodylewis.com>
2015-10-21 10:26:56 -0400

    Working pagination, need to set min/max

--- src/js/controllers/mainCtrl.js
+++ src/js/controllers/mainCtrl.js
@@ -10,13 +10,44 @@ angular
 function MainCtrl($scope, $state, PhotoEngine) {
   var vm = this;
   vm.photos = [];
+  vm.page = [];
+  vm.pageIndex = 0;
+  vm.pageCount = 10;
   vm.currentPhoto = {};
   vm.showModal = false;
   vm.pe = new PhotoEngine();
   vm.pe.fetchPhotos()
   .then(function(data){
     parseFlickrResponse(data);
+  })
+  .then(function() {
+    vm.init();
   });
+
+  vm.init = function() {
+    for(var i = vm.pageIndex; i < (vm.pageIndex + 10); i++) {
+      vm.page.push(vm.photos[i]);
+    }
+        vm.pageIndex += vm.pageCount;
+  };
+
+  vm.photoPage = function(direction) {
+    for(var i = vm.pageIndex; i < (vm.pageIndex + 10); i++) {
+      vm.page.push(vm.photos[i]);
+    }
+      if (direction === 'forward') {
+        vm.pageIndex += vm.pageCount;
+      }
+      else if (direction === 'back') {
+        vm.pageIndex -= vm.pageCount;
+      }   
+      console.log(vm.pageIndex);
+  };
+  
+  vm.changePage = function(direction) {
+    vm.page = [];
+    vm.photoPage(direction);
+  };
 
   vm.toggleModal = function(photo) {
     vm.showModal = !vm.showModal;
@@ -37,7 +68,6 @@ function MainCtrl($scope, $state, PhotoEngine) {
         for(var i = 0; i < vm.photos.length; i++) {
           vm.photos[i].thumb = 'https://farm' + vm.photos[i].farm + '.staticflickr.com/' + vm.photos[i].server + '/' + vm.photos[i].id + '_' + vm.photos[i].secret + '_s.jpg';
           vm.photos[i].medium = 'https://farm' + vm.photos[i].farm + '.staticflickr.com/' + vm.photos[i].server + '/' + vm.photos[i].id + '_' + vm.photos[i].secret + '_z.jpg';
-          //console.log(vm.photos[i].thumb);
         }
   }
 }
